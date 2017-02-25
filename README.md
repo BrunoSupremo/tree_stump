e55eb9fd568591ac401dd05869437cc0d779d9d7
Bruno <bruno_mussoi@hotmail.com>
2017-02-25 11:13:20 -0300

    First

---
+++ README.md
@@ -0,0 +1,5 @@
+## Tree Stump
+
+This mod (for the game Stonehearth) adds a new mechanic.
+
+With this mod, trees now does not just disappear after being cut. Instead, they will leave a small stump of itself behind, which can also be cut later.
---
+++ components/add_stump.lua
@@ -0,0 +1,42 @@
+local StumpComponent = class()
+
+function StumpComponent:activate()
+   self._stump_data = radiant.entities.get_entity_data(self._entity, 'tree_stump:stump_data')
+   if self._stump_data then
+      self._on_harvest_listener = radiant.events.listen(self._entity, 'stonehearth:kill_event', function()
+            self:add_stump()
+            self._on_harvest_listener = nil
+         end)
+   end
+end
+
+function StumpComponent:destroy()
+   if self._on_harvest_listener then
+      self._on_harvest_listener:destroy()
+      self._on_harvest_listener = nil
+   end
+end
+
+function StumpComponent:add_stump()
+   local location = radiant.entities.get_world_grid_location(self._entity)
+   if not location then
+      --destroy???-------------------------
+      return
+   end
+   
+   --Create the entity and put it on the ground
+   if not self._stump_data.stump_alias then
+      return
+   end
+   local the_stump = radiant.entities.create_entity(self._stump_data.stump_alias, {})
+   if not the_stump then
+      return
+   end
+   radiant.terrain.place_entity_at_exact_location(the_stump, location)
+
+   --turn it to correct rotation
+   local rotation = self._entity:get_component('mob'):get_facing() +180 % 360 --spin 180
+   radiant.entities.turn_to(the_stump, rotation)
+end
+
+return StumpComponent
