d83a5a1aebebc7c954fdc08b95901897a203b1bf
Bruno <bruno_mussoi@hotmail.com>
2018-12-20 16:54:49 -0200

    swamp trees added

--- server.lua
+++ server.lua
@@ -1,7 +1,7 @@
 tree_stump = {}
 
 function tree_stump:_on_required_loaded()
-	local ace_add_stump = radiant.mods.require('stonehearth_ace.components.tree_stump.add_stump')
+	local ace_add_stump = radiant.mods.require('stonehearth_ace.components.stump.stump_component')
 	if ace_add_stump then
 		ace_add_stump.add_stump = function() return end
 	end
